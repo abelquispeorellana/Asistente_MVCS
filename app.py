6aef7557f7c372a3f3d6da51ffd3e2b59f3116d6
Developer <Developer>
2026-05-14 12:29:11 -0500

    fix: revertir pysqlite3 y protobuf pin que crasheaban el arranque

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
-__import__('pysqlite3')
 import sys, os
-sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 os.environ["PROTOCOL_BUFFERS_PYTHON_IMPLEMENTATION"] = "python"
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), "src"))
 
